e684eca8ce45eeaace18e294ee475bdd1e7e640e
simsmile123 <76024949+simsmile123@users.noreply.github.com>
2021-04-10 19:58:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 //index.js
-
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#input").addEventListener("keydown", function(e) {
-    if (e.code === "Enter") {
-        console.log("You clicked the form and pressed the enter button!")
+    const inputField = document.getElementById("input")
+    inputField.addEventListener("keydown", function(e) {
+        if (e.code === "Enter") {
+            let input = inputField.value;
+            inputField.value = "";
+            output(input);
     }
   });
 });
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.querySelector("#input").addEventListener("keydown", function(e) {
+//     if (e.code === "Enter") {
+//         console.log("You clicked the form and pressed the enter button!")
+//     }
+//   });
+// });
