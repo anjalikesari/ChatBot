900f8aae011e47648ee813283a3b622ad9ddafdd
simsmile123 <76024949+simsmile123@users.noreply.github.com>
2021-04-10 19:53:45 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+//index.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#input").addEventListener("keydown", function(e) {
+    if (e.code === "Enter") {
+        console.log("You clicked the form and pressed the enter button!")
+    }
+  });
+});
