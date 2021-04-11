f369f1375a6e16c5bf986df7c71e0ebf4b827fa3
simsmile123 <76024949+simsmile123@users.noreply.github.com>
2021-04-10 21:07:18 -0400

    Update constants.js

--- constants.js
+++ constants.js
@@ -59,7 +59,7 @@ const replies = [
   ["That's ok","I understand","What do you want to talk about?"],
   ["Please say something :("],
   ["Haha!","Good one!"],
-  ["don't say mean words!"]
+  ["don't say mean words jay!"]
 ]
 
 // Random for any other user input
