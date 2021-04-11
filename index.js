7c2cb788b293d45561764a138e67af49af323674
anjalikesari <45514629+anjalikesari@users.noreply.github.com>
2021-04-10 20:37:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function addChat(input, product) {
   let botImg = document.createElement("img");
   let botText = document.createElement("span");
   botDiv.id = "bot";
-  botImg.src = "bot-mini.png";
+  botImg.src = "chatbot.png";
   botImg.className = "avatar";
   botDiv.className = "bot response";
   botText.innerText = "Typing...";
